3c85306da8aec61955dadcb4fa5060dbc2892e5e
fahrooo <fahrooo2808@student.uns.ac.id>
2023-01-14 01:39:33 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import { Box, Stack, Text } from "@chakra-ui/layout";
 import { Switch } from "@chakra-ui/switch";
 import { useState } from "react";
 import axios from "axios";
+import logo from "../assets/images/logo.png";
+import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 
 export default function Home() {
   const [lampu, setLampu] = useState(true);
@@ -34,12 +36,95 @@ export default function Home() {
       <main>
         <Box
           w="100%"
+          h="400"
+          bgGradient="linear(to-b, #355D77 20%, rgba(150, 155, 158, 0.389207), rgba(91, 118, 136, 0))"
+          position="absolute"
+          borderBottomRadius="50%"
+          filter="auto"
+          blur="2px"
+        />
+        <Box
+          bgImage="url('/_next/static/media/kelas.ec384f45.png')"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          bgSize="cover"
           h="100vh"
           display="flex"
           alignItems="center"
           justifyContent="center"
         >
-          <Stack
+          <Box position="absolute">
+            <Box mb="50px" display="flex" justifyContent="center">
+              <Stack direction="column" alignItems="center">
+                <Image src={logo} alt="logo" />
+                <Text fontSize="15px" color="#FFFFFF" fontWeight="bold">
+                  Innovation Connect
+                </Text>
+              </Stack>
+            </Box>
+            <Box w="350px" h="440px" bgColor="#393D43" borderRadius="40px">
+              <Box display="flex" justifyContent="center">
+                <Text
+                  fontSize="27px"
+                  color="#FFFFFF"
+                  mt="33px"
+                  fontWeight="700"
+                >
+                  Log In
+                </Text>
+              </Box>
+              <Box m="25px">
+                <Box>
+                  <FormControl>
+                    <FormLabel fontSize="15px" color="#FFFFFF">
+                      Email
+                    </FormLabel>
+                    <Input
+                      type="email"
+                      h="35px"
+                      bgColor="#D9D9D9"
+                      borderRadius="15px"
+                      fontWeight="400"
+                    />
+                  </FormControl>
+                </Box>
+                <Box mt="20px">
+                  <FormControl>
+                    <FormLabel fontSize="15px" color="#FFFFFF">
+                      Password
+                    </FormLabel>
+                    <Input
+                      type="password"
+                      h="35px"
+                      bgColor="#D9D9D9"
+                      borderRadius="15px"
+                      fontWeight="400"
+                    />
+                  </FormControl>
+                </Box>
+                <Box mt="20px" display="flex" justifyContent="end">
+                  <Text fontSize="13px" color="#FFFFFF" cursor="pointer">
+                    Lupa password ?
+                  </Text>
+                </Box>
+                <Box w="100%" mt="20px">
+                  <Button w="100%" colorScheme="messenger">
+                    Log In
+                  </Button>
+                </Box>
+                <Box mt="20px" display="flex" justifyContent="start">
+                  <Text fontSize="13px" color="#FFFFFF">
+                    Belum memiliki akun?
+                  </Text>
+                  <Text ms="5px" fontSize="13px" color="#FF9567" cursor="pointer">
+                    Silahkan Register
+                  </Text>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          {/* <Image src={kelas} alt="kelas" /> */}
+          {/* <Stack
             border="1px"
             borderColor="gray.200"
             alignItems="center"
@@ -52,7 +137,7 @@ export default function Home() {
             <Text fontSize="xl" color="gray.900">
               {lampu ? "off" : "on"}
             </Text>
-          </Stack>
+          </Stack> */}
         </Box>
       </main>
     </>
