32de3ddb0e6c5c44699d5d9e8a9784210c965f2f
fahrooo <fahrooo2808@student.uns.ac.id>
2023-01-13 17:00:57 +0700

    up

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function App({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+}
