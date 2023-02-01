6ba5a1a9afe076582ef8559aa9482fbf39926481
fahrooo <fahrooo2808@student.uns.ac.id>
2023-02-01 16:37:46 +0700

    up

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
 
   return (
     <ChakraProvider>
+      <NextNProgress
+        height={5}
+        color="#6994C9"
+        options={{ showSpinner: false }}
+      />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
